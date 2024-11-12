d3248bc48e749560ff16ed6222d4ceb7810c5bfd
M-Omar-401 <muhammd.omar401@gmail.com>
2024-11-12 09:43:54 +0500

    hello my first next js committ

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <>
+  <div>
+    {/* <i class="fas fa-h1    ">Hello I am Muhammad Umar</i> */}
+    <h1>Hello I am Muhammad Umar</h1>
+  </div>
+  </>
+  );
+}
